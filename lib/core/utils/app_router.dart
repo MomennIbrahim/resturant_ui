6f47dde23666aa1959961eda70fee45281128ef1
unknown <ibrahimmomen179@gmail.com>
2023-08-30 13:20:36 +0300

    added new route

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -5,15 +5,21 @@ import 'package:resturant_ui_app/features/onBoarding/presentation/on_boarding_sc
 
 abstract class AppRouter {
   static const kAuthenticationScreen = '/loginScreen';
+  static const kLayoutScreen = '/layoutScreen';
 
   static final router = GoRouter(routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const LayoutScreen(),
+      builder: (context, state) => const OnBoaringScreen(),
     ),
     GoRoute(
       path: kAuthenticationScreen,
       builder: (context, state) => const AuthenticationScreen(),
+    ),
+
+    GoRoute(
+      path: kLayoutScreen,
+      builder: (context, state) => const LayoutScreen(),
     ),
   ]);
 }
--- lib/features/authentication/presentation/views/widgets/register_screen.dart
+++ lib/features/authentication/presentation/views/widgets/register_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:resturant_ui_app/core/utils/app_router.dart';
 import 'package:resturant_ui_app/core/widgets/custom_button.dart';
 import 'package:resturant_ui_app/core/widgets/text_over_text_form_field.dart';
 import 'package:resturant_ui_app/features/authentication/presentation/views/widgets/email_text_field.dart';
@@ -51,7 +53,9 @@ class RegisterScreen extends StatelessWidget {
             ),
             CustomElevatedButton(
               text: 'Registration',
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.kLayoutScreen);
+              },
             ),
             const SizedBox(
               height: 16.0,
