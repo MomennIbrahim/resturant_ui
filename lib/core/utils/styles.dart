1f3635a0c4a4a8d51fa1fa51df233b1ce0e06c52
unknown <ibrahimmomen179@gmail.com>
2023-09-03 14:49:16 +0300

    finished profile scren

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -22,6 +22,9 @@ abstract class Styles {
   static const style16 = TextStyle(
       fontSize: 16.0, fontWeight: FontWeight.w400, color: Color(0xff4B5563));
 
+  static const style18 = TextStyle(
+      fontSize: 18.0, fontWeight: FontWeight.w400, color: Color(0xff374151));
+
   static const style24 = TextStyle(
     fontSize: 24.0,
     fontWeight: FontWeight.w600,
